cb45bf5ce4aa933bcdb221707e1896cf46bca6f2
binwu-zhang <13522943697@163.com>
2022-06-16 21:38:27 +0800

    init

---
+++ springMVC-demo3/src/main/java/com/atguigu/mvc/controller/ViewController.java
@@ -0,0 +1,26 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping("/testThymeleafView")
+    public String testThymeleafView(){
+        return "success";
+    }
+
+    //转发
+    @RequestMapping("/testForward")
+    public String testForward(){
+        return "forward:/testThymeleafView";
+    }
+
+    //重定向
+    @RequestMapping("/testRedirect")
+    public String testRedirect(){
+        return "redirect:/testThymeleafView";
+    }
+
+}
---
+++ springMVC-rest/src/main/java/com/atguigu/mvc/bean/Employee.java
@@ -0,0 +1,51 @@
+package com.atguigu.mvc.bean;
+
+public class Employee {
+    private Integer id;
+    private String lastName;
+
+    private String email;
+    private Integer gender;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Employee(Integer id, String lastName, String email, Integer gender) {
+        this.id = id;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public Employee() {
+    }
+}
